712299309ddfd5643545b8380da65139016d6899
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-08-01 17:03:22 +0500

    Notification’ga qarab turli xabar chiqarish

--- compolier/index.js
+++ compolier/index.js
@@ -12,16 +12,26 @@
 //   { id: 2, name: 'Laylo', email: 'laylo@mail.com', isActive: false },
 //   { id: 3, name: 'Doston', email: 'doston@example.com', isActive: true },
 // ];
-// 👇 Bu yerga funksiya yozing
-function sendNotification(notification) {
-    if (notification.type === 'email') {
-        console.log(`Email orqali yuborilmoqda: ${notification.email}`);
-    }
-    else if (notification.type === 'sms') {
-        console.log(`SMS yuborilmoqda: ${notification.phone}`);
-    }
-    else if (notification.type === 'push') {
-        console.log(`Push bildirishnoma yuborilmoqda: ${notification.deviceId}`);
-    }
-}
-sendNotification({ type: 'email', email: 'user@example.com' });
+// function activeUsers(users: Users[]): Users[] {
+//   return users.filter((user) => user.isActive);
+// }
+// console.log(activeUsers(users));
+//==========================================
+// 2-MASALA: Notification’ga qarab turli xabar chiqarish
+// 👇 Shart:
+// Quyidagi Notification tipidan foydalangan holda funksiya yozing. Funksiya har xil turdagi xabarlarni qabul qiladi va qaysi kanal orqali yuborilayotganini konsolga chiqaradi.
+// type CustomNotification =
+//   | { type: 'email'; email: string }
+//   | { type: 'sms'; phone: string }
+//   | { type: 'push'; deviceId: string };
+// // 👇 Bu yerga funksiya yozing
+// function sendNotification(notification: CustomNotification): void {
+//   if (notification.type === 'email') {
+//     console.log(`Email orqali yuborilmoqda: ${notification.email}`);
+//   } else if (notification.type === 'sms') {
+//     console.log(`SMS yuborilmoqda: ${notification.phone}`);
+//   } else if (notification.type === 'push') {
+//     console.log(`Push bildirishnoma yuborilmoqda: ${notification.deviceId}`);
+//   }
+// }
+// sendNotification({ type: 'email', email: 'user@example.com' });
