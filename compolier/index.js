39a785517acc002e093dc086ed8e4197e9cb044a
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-08-01 16:55:20 +0500

    Type yarat va faqat active userlarni ajratib oladigan funksiya yoz

---
+++ compolier/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// API'dan keladigan userlar ro'yxati:
+// Vazifa: Type yarat va faqat active userlarni ajratib oladigan funksiya yoz
+// type Users = {
+//   id: number;
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+// const users: Users[] = [
+//   { id: 1, name: 'Ali', email: 'ali@gmail.com', isActive: true },
+//   { id: 2, name: 'Laylo', email: 'laylo@mail.com', isActive: false },
+//   { id: 3, name: 'Doston', email: 'doston@example.com', isActive: true },
+// ];
+// function activeUsers(users: Users[]): Users[] {
+//   return users.filter((user) => user.isActive);
+// }
+// console.log(activeUsers(users));
